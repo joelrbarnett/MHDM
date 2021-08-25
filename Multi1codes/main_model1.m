77a000df053be7645fe44ca4e061a8e55bdffa6e
Joel Barnett <joelrbarnett@gmail.com>
2021-08-25 12:22:04 -0700

    remerge multi1codes

--- Multi1codes/main_model1.m
+++ Multi1codes/main_model1.m
@@ -18,48 +18,54 @@ disp('====================================')
 
 %% Our method
 folder_path="../Test_Images_plus1/"; %read images with no zero values
-fileNames=["barbara","cameraman","pollen","mandril","circles","geometry","disc_square"]; 
+fileNames=["barbara","cameraman","mandril","disc_square"]; 
 images=["barbara.png","cameraman.tif","pollen.tif","mandril_gray.tif","circles.tif","geometry.tif","disc_square.png"];
-imagesPNG=["barbara.png","cameraman.png","pollen.png","mandril.png","circles.png","geometry.png","disc_square.png"];
-
-f_orig=double(imread(char(folder_path+imagesPNG(4))));
-%%%Gamma noise %%%
-rng(10); %fix seed across all runs
-a=25; %gamma noise with mean 1, standard deviation 0.2. 
-GamNoise=gamrnd(a,1/a,size(f_orig));
-f_data=f_orig.*GamNoise;
-
-lambda=0.06; sigma=3; tau=3; alpha=16; %20*sqrt(6)/9;
-[lambda,sigma,tau,alpha]
-options.bound = 'per';
-maxit=1000;
-
-%Find the best lambda to use
-% lambdaArray=0.05:0.01:0.2;
-% snrArray=zeros(size(lambdaArray));
-% iter=1;
-% for lambda = lambdaArray
-%     tic;
-%     %un=DZconvex1_cpu(f,sigma,lambda,alpha,options,tau,179,x,y);
-%     [un,fun_value,diff,it]=DZconvex1(f_data,sigma,lambda,alpha,options,tau,maxit,f_orig,f_data);
-%     t=toc
-%     snrArray(iter)=20*log10(norm(un,'fro')/norm(un-f_orig,'fro'));
-%     iter=iter+1;
-% end
-
-%Get the best snr value, and use that lambda for final run
-% [~,maxIt]=max(snrArray);
-% lambda=lambdaArray(maxIt);
+imagesPNG=["barbara.png","cameraman.png","mandril.png","disc_square.png"];
 
 
+for k = 2:length(imagesPNG)
+    f_orig=double(imread(char(folder_path+imagesPNG(k))));
+    %%%Gamma noise %%%
+    rng(10); %fix seed across all runs
+    a=25; %gamma noise with mean 1, standard deviation 0.2. 
+    GamNoise=gamrnd(a,1/a,size(f_orig));
+    f_data=f_orig.*GamNoise;
 
-tic;
-%un=DZconvex1_cpu(f,sigma,lambda,alpha,options,tau,179,x,y);
-[un,fun_value,diff,it]=DZconvex1(f_data,sigma,lambda,alpha,options,tau,maxit,f_orig,f_data);
-t=toc
-figure,imshow(uint8(un));
+    sigma=3; tau=3; alpha=16; %20*sqrt(6)/9;
+    options.bound = 'per';
+    maxit=1000;
+
+    %Find the best lambda to use
+    lambdaArray=0.05:0.01:0.2;
+    snrArray=zeros(size(lambdaArray));
+    iter=1;
+    for lambda = lambdaArray
+        tic;
+        %un=DZconvex1_cpu(f,sigma,lambda,alpha,options,tau,179,x,y);
+        [un,fun_value,diff,it]=DZconvex1(f_data,sigma,lambda,alpha,options,tau,maxit,f_orig,f_data);
+        t=toc
+        snrArray(iter)=20*log10(norm(un,'fro')/norm(un-f_orig,'fro'));
+        iter=iter+1;
+    end
+
+    %Get the best snr value, and use that lambda for final run
+    [~,maxIt]=max(snrArray);
+    lambda=lambdaArray(maxIt);
+    [un,fun_value,diff,it]=DZconvex1(f_data,sigma,lambda,alpha,options,tau,maxit,f_orig,f_data);
+    filename="dong_"+imagesPNG(k);
+    imwrite(uint8(un),char(filename))
+    snrs=20*log10(norm(un,'fro')/norm(un-f_orig,'fro'));
+    save("vars_dong_"+fileNames(k),'f_orig','f_data','un','lambda','sigma','alpha','options','tau','maxit','snrs');
+
+end
+
+% tic;
+% %un=DZconvex1_cpu(f,sigma,lambda,alpha,options,tau,179,x,y);
+% [un,fun_value,diff,it]=DZconvex1(f_data,sigma,lambda,alpha,options,tau,maxit,f_orig,f_data);
+% t=toc
+%figure,imshow(uint8(un));
 %psnr(f_orig,min(un,255))
 %real snr 
-20*log10(norm(un,'fro')/norm(un-f_orig,'fro'))
-figure,plot((2:it)),xlabel('iterations'),ylabel('function values')
+%20*log10(norm(un,'fro')/norm(un-f_orig,'fro'))
+%figure,plot((2:it)),xlabel('iterations'),ylabel('function values')
 
